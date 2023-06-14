8106f0062083037952d1a4f995573e5e3b23391d
Mubasshirah <adibaju1994@gmail.com>
2023-06-14 19:11:48 +0600

    user post operation

--- index.js
+++ index.js
@@ -8,6 +8,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// verifyJWT
+const verifyJWT=(req,res,next)=>{
+  const authorization=req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error:true,message:'unauthorized access'});
+  }
+
+  const token=authorization.split(' ')[1];
+  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+    if(err){
+      return res.status(401).send({error:true,message:'unauthorized and cant decode'})
+    }
+    req.decoded=decoded;
+    next();
+  })
+}
 
 
 const {
@@ -30,6 +46,7 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const classCollection = client.db('craftSchool').collection('classes');
+    const userCollection = client.db('craftSchool').collection('users');
 
     // classCollection
     app.get('/classes', async (req, res) => {
@@ -40,6 +57,18 @@ async function run() {
     })
 
     // classCollection
+    // userCollection
+    app.post('/users',async(req,res)=>{
+      const user=req.body;
+      const query={email:user.email};
+      const existing=await userCollection.findOne(query);
+      if(existing){
+        return res.send({message:'user already exist'})
+      }
+      const result=await userCollection.insertOne(user);
+      res.send(result);
+    })
+    // userCollection
     // jwt
     app.post('/jwt', (req, res) => {
       const user = req.body;
